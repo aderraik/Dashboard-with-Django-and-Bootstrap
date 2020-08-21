8d8335ce70743a5e7e79114e159b7f8aa890e7dd
Andre B. Derraik <aderraik@gmail.com>
2020-08-21 12:28:09 -0300

    Adding new logins urls

--- dashboard/apps/urls.py
+++ dashboard/apps/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from django.urls.conf import include
 
 from dashboard.apps.frontend.views import IndexView
+from dashboard.apps.pages.login.views import LoginView, GoogleView
 
 app_name = 'app'
 
 urlpatterns = [
-    path('', 				IndexView.as_view(),						name='index'),
+    path('', 				IndexView.as_view(),	name='index'),
+    path('login',			LoginView.as_view(),	name='login'),
+    path('google',			GoogleView.as_view(),	name='google'),
 
     path('pages/',			include('dashboard.apps.pages.urls')),
     path('components/',		include('dashboard.apps.components.urls')),
