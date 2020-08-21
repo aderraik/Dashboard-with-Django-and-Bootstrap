5cff5fca3e76ff8b1212525611f2533a72db8497
Andre B. Derraik <aderraik@gmail.com>
2020-08-21 12:27:27 -0300

    Adding account and google logins view

--- dashboard/apps/pages/login/views.py
+++ dashboard/apps/pages/login/views.py
@@ -4,11 +4,53 @@ from django.views import generic
 from dashboard.apps.core.utils import log
 
 
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     """
     IndexView:
     """
     module = 'IndexView'
+    template_name = 'login/base.html'
+
+    data = None
+
+    def get_queryset(self):
+        log(self.module, 'get_queryset', file=__file__)
+
+        return self.data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = self.data
+
+        return context
+
+
+class LoginView(generic.TemplateView):
+    """
+    LoginView:
+    """
+    module = 'LoginView'
+    template_name = 'login/base.html'
+
+    data = None
+
+    def get_queryset(self):
+        log(self.module, 'get_queryset', file=__file__)
+
+        return self.data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = self.data
+
+        return context
+
+
+class GoogleView(generic.TemplateView):
+    """
+    GoogleView:
+    """
+    module = 'GoogleView'
     template_name = 'login/base.html'
 
     data = None
